4df8d98d7128075b72b3ad2004a248fd4bd97517
Arturo Jaldo <qp.artnitro@gmail.com>
2019-06-12 18:37:46 +0200

    Desarrollo parcial changepwd component

--- public/src/changepwd/changepwd.component.ts
+++ public/src/changepwd/changepwd.component.ts
@@ -3,11 +3,15 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LocalStorageService } from 'ngx-webstorage';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { TranslateService } from '@ngx-translate/core';
 
-import { LOCAL } from '../app.config';
+import { COLORS, LOCAL, LANGUAGE } from '../app.config';
+import { AFields } from '../services/form/AFields';
+import { ConfirmPasswordValidator } from '../services/form/validators/confirmpassword.validator';
 
 const JWT = new JwtHelperService();
 
@@ -16,24 +20,71 @@ const JWT = new JwtHelperService();
   templateUrl: './changepwd.html'
 })
 
-export class ChangepwdComponent {
+export class ChangepwdComponent extends AFields implements OnInit {
+
+  changepwdForm: FormGroup;
 
   token: string;
   decodedToken: object;
+  hasError: object = {};
+  formColor: string;
+  formMessage: string;
+  formMessageColor: string;
+  show: boolean = true;
 
   constructor(
+    private fb: FormBuilder,
     private router: Router,
-    private localStorage: LocalStorageService
+    private localStorage: LocalStorageService,
+    private translate: TranslateService
   ) {
+    super();
     console.info('>>>>> Changepwd component.');
+    this.translate.setDefaultLang(LANGUAGE.defaultLanguage);
     this.token = this.localStorage.retrieve(LOCAL.forgotPwd);
-    this.decodedToken = JWT.decodeToken(this.token);
   }
 
   ngOnInit() {
-    if ( this.token === null || JWT.isTokenExpired(this.token) ) {
-      this.router.navigate(['/signin']);
-    }
+    this.decodedToken = ( this.token === null || JWT.isTokenExpired(this.token) )
+      ? (() => { this.router.navigate(['/signin']); })()
+      : JWT.decodeToken(this.token);
+    this.changepwdForm = this.fb.group({
+      password: this.password(),
+      confirmPassword: ['']
+    }, {
+      validator: ConfirmPasswordValidator.MatchPassword
+    });
+    this.translate.get('CHANGEPWD.STATUS1').subscribe((res: string) => {
+      this.formMessage = res;
+    });
+  }
+
+  changepwd() {
+    this.hasError = this.checkFields(this.changepwdForm);
+    (Object.keys(this.hasError).length)
+      ? this.typingData(COLORS.red, 'CHANGEPWD.ERROR1')
+      :
+        (
+          this.typingData(COLORS.black, 'CHANGEPWD.STATUS2'),
+          this.sendData()
+        )
+  }
+
+  //TODO: No olvidar incluir email que recojo de decodedToken :)
+  sendData() {
+    console.log('>>> Sending data');
+  }
+
+  typingData(colors: string, text: string) {
+    this.translate.get(text).subscribe((res: string) => {
+      this.formMessage = res;
+      this.formMessageColor = colors;
+      this.formColor = colors;
+    });
+  }
+
+  changeContinue() {
+    this.router.navigate(['/signin']);
   }
 
 }
