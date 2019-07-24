d59b2bc455083ab9f407f2c867134968935a6274
Arturo Jaldo <qp.artnitro@gmail.com>
2019-07-24 20:03:45 +0200

    Agrego nuevo componente an-input

--- public/src/signup/signup.module.ts
+++ public/src/signup/signup.module.ts
@@ -10,6 +10,7 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { RoutingModule } from './signup.routing';
 import { MessageModule } from '../components/message/message.module';
+import { InputModule } from '../components/input/input.module';
 import { TranslateLazyConfigModule } from '../translate.lazy.config.module';
 
 import { SignupComponent } from './signup.component';
@@ -23,7 +24,8 @@ import { SignupService } from './signup.service';
     TranslateModule,
     TranslateLazyConfigModule,
     RoutingModule,
-    MessageModule
+    MessageModule,
+    InputModule
   ],
   providers: [SignupService],
   declarations: [SignupComponent]
