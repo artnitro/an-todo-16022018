c6ac977f4840dad04799e7518836180958586b59
Arturo Jaldo <qp.artnitro@gmail.com>
2019-08-02 19:13:00 +0200

    Agrego accesso para opciones de Dashboard

--- public/src/dashboard/dashboard.module.ts
+++ public/src/dashboard/dashboard.module.ts
@@ -3,11 +3,16 @@
  */
 
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports: [], 
+  imports: [
+    CommonModule,
+    RouterModule
+  ], 
   providers: [],
   declarations: [DashboardComponent],
   exports: [DashboardComponent]
