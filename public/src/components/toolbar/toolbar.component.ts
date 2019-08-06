6971a6c9fe8b4d56afecd3028f903f80fa136a1b
Arturo Jaldo <qp.artnitro@gmail.com>
2019-08-06 19:24:37 +0200

    Agrego nuevo componente toolbar horizontal

---
+++ public/src/components/toolbar/toolbar.component.ts
@@ -0,0 +1,16 @@
+/**
+ * an-toolbar component.
+ * 
+ * <an-toolbar-horizotal></an-toolbar-horizontal>
+ */
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'an-toolbar-horizontal',
+  templateUrl: './toolbar.html'
+})
+
+export class ToolbarComponent {
+  
+}
