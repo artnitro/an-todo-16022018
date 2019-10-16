90dd308f0469a92f79f37dc10383a9658a57aed1
Arturo Jaldo <qp.artnitro@gmail.com>
2019-10-16 20:40:45 +0200

    Validación de Authoritation y configuración Subscription para Apollo Server

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,16 +4,47 @@
 
 import 'reflect-metadata';
 import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer, ApolloError } from 'apollo-server-express';
+import http from 'http';
+import jwt from 'jsonwebtoken';
 
 import { typeDefs } from './typedefs';
 import { resolvers } from './resolvers';
 
 const app: express.Application = express();
-const server = new ApolloServer({ typeDefs, resolvers });
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  subscriptions: {
+    path: '/api/v1/', 
+    onConnect: ( connectionParams, webSokect, context ) => {
+      // connectionParams son los parámetros que se envían de apollo client en la conexión websocket.
+      // webSocket es la información enviada al servidor y propiedades de este servidor.
+      console.log('>>> Connected websocker.');
+    }, 
+    onDisconnect: (webSocket, context) => {
+      console.log('>>> Disconnected websocket');
+    }
+  },
+  context: ({ req, connection }) => {
+    try {
+      // TODO: Verificar connection cuando tenga subscriptions, ya que se gestiona el contexto a través de connection con subscription.
+      const token = ( req.headers.authoritation as string ).substring( req.headers.authoritation.indexOf(' ') + 1, req.headers.authoritation.length );
+      const user = jwt.verify(token, process.env.JWT_SECRET);
+      return { user };
+    } catch (error) {
+      console.log('>>> Error: ', error);
+      throw new ApolloError('Unauthorized User', '401');
+    }
+  }
+});
 
 server.applyMiddleware({ app, path: '/api/v1/' });
+server.installSubscriptionHandlers(httpServer);
 
-app.listen({ port: process.env.PORT }, () => {
-  console.log(`>>> Server says: Bootstrapping server, listening port: ${process.env.PORT}`);  
+httpServer.listen({ port: process.env.PORT }, () => {
+  console.log(`>>> Server says: Bootstraping server, listening port: ${process.env.PORT}`);
+  console.log(`>>> Server says: Subscriptions ready, listening port: ${process.env.PORT}`);
 });
