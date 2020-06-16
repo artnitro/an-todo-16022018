b3c194e96207f05c6a56987592ad8e393bd05ee4
Arturo Jaldo <qp.artnitro@gmail.com>
2020-06-16 20:00:18 +0200

    Elimino morralla innecesaria del fichero

--- api/src/graphql/resolvers.ts
+++ api/src/graphql/resolvers.ts
@@ -2,28 +2,9 @@
  * Resolvers for GraphQL
  */
 
-// Carga de modelos para ver si da error. 
+import merge from 'deepmerge';
 
-import { UserModel } from '../model/user.model';
-import { ProjectModel } from '../model/project.model';
-
-// console.log('Modelo User: ', UserModel);
-// const Proyecto = ProjectModel('an-project');
-// console.log('Modelo Proyecto:', Proyecto);
-
-///////////////////////////////////////
-
-// Carga dinámica de resolvers, query.
-
-import merge from 'deepmerge'; //// Para unir objetos y arrays.
 import { Query } from './query/query.resolver';
-
-
-// console.log('>>> Resolver with Query: ', Query);
 
 export const resolvers = Object.create(Object.prototype);
 resolvers.Query = Query;
-
-// console.log('>>> Resolvers: ', resolvers);
-
-////////////////////////////////////
