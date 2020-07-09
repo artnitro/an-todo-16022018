5127388bdd8f69e63b485b111c2f83196039a3bb
Arturo Jaldo <qp.artnitro@gmail.com>
2020-07-09 18:39:48 +0200

    Agrego Mutarion a los resolvers

--- api/src/graphql/resolvers.ts
+++ api/src/graphql/resolvers.ts
@@ -5,6 +5,10 @@
 import merge from 'deepmerge';
 
 import { Query } from './query/query.resolver';
+import { Mutation } from './mutation/mutation.resolver';
+
 
 export const resolvers = Object.create(Object.prototype);
+
 resolvers.Query = Query;
+resolvers.Mutation = Mutation;
