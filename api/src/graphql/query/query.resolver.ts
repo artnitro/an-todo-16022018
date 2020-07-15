e4ea295260d34c2567beb2eea21432d5381347e5
Arturo Jaldo <qp.artnitro@gmail.com>
2020-07-15 20:03:34 +0200

    Defino dos nuevas Queries de GraphQl

--- api/src/graphql/query/query.resolver.ts
+++ api/src/graphql/query/query.resolver.ts
@@ -6,10 +6,12 @@ import merge from 'deepmerge';
 
 import { UserProjects } from './userprojects.resolver';
 import { UserData } from './userdata.resolver';
+import { GetCollections } from './getcollections.resolver';
 
 export const Query = merge.all([ 
   UserProjects,
-  UserData
+  UserData,
+  GetCollections
 ]);
 
 // console.log('>>> typeOf:', typeof Query);
